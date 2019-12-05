e08cdb8ada492ee75ac88356887be9e2cdb14632
lucas2509 <58375333+lucas2509@users.noreply.github.com>
2019-12-05 15:12:05 -0300

    Update versao3.asm

--- versao3.asm
+++ versao3.asm
@@ -1,9 +1,10 @@
 .text
 	.globl Inicio
 	
-	Inicio: #display em 4,2,512,512	
+	Inicio: #display em 4,2,512,512		
 		li $t0,0
-		li $t1,1000
+		li $t1,2000
+		
 		
 		loop_inicio:
 		slt $t2,$t0,$t1
@@ -124,7 +125,18 @@
 		move $v0,$t5
 		jal Apagar_bomba
 		
-		cont4_loop_confronto:																
+		cont4_loop_confronto:
+		li $t6,5
+		bne $t4,$t6,cont5_loop_confronto		
+		li $t6,130560
+		slt $t6,$t5,$t6
+		bne $t6,$zero,cont5_loop_confronto		
+		li $t6,0
+		sw $t6,0($t2)
+		move $v0,$t5
+		jal Apagar_moeda
+		
+		cont5_loop_confronto:																
 		addi $t0,$t0,1
 		addi $t2,$t2,4
 		addi $t3,$t3,4
@@ -147,7 +159,7 @@
 		mul $a0,$a0,4
 		move $t0,$a0
 		
-		li $a1,4
+		li $a1,5
 		li $v0,42
 		syscall
 		addi $a0,$a0,1	
@@ -247,6 +259,13 @@
 		jal Apagar_bomba
 		
 		cont4_loop_apagfrutas:
+		li $t5,5
+		bne $t4,$t5,cont5_loop_apagfrutas
+		lw $v0,0($t3)
+		jal Apagar_moeda
+		
+		cont5_loop_apagfrutas:
+		
 		addi $t0,$t0,1
 		addi $t2,$t2,4
 		addi $t3,$t3,4
@@ -258,7 +277,7 @@
 		jr  $ra
         #########################################
 	Apagar_tela:
-		li $t0,0x00000000
+		li $t0,0x332299
 		move $t1,$gp
 		addi $t2,$t1,131072
 		
@@ -314,7 +333,15 @@
 		move $v0,$t5				
 		jal Pintar_bomba
 		
-		continue4_loop_pintatela:			
+		continue4_loop_pintatela:
+		li $t5,5
+		bne $t4,$t5,continue5_loop_pintatela			
+		lw $t5,0($t6)		
+		move $v0,$t5				
+		jal Pintar_moeda
+		
+		continue5_loop_pintatela:
+								
 		addi $t6,$t6,4
 		addi $t2,$t2,4
 		addi $t0,$t0,1
@@ -1544,6 +1571,280 @@
 
 
 
+	lw $t0,0($sp)
+	lw $t1,4($sp)
+	lw $t2,8($sp)
+	lw $t3,12($sp)
+	lw $t4,16($sp)
+	lw $t5,20($sp)
+	lw $t6,24($sp)
+	lw $t7,28($sp)
+	addi $sp,$sp,32
+	jr $ra
+	
+	###### Pintar Moeda ############
+	Pintar_moeda:
+	#maça
+	addi $sp,$sp,-32
+	sw $t0,0($sp)
+	sw $t1,4($sp)
+	sw $t2,8($sp)
+	sw $t3,12($sp)
+	sw $t4,16($sp)
+	sw $t5,20($sp)
+	sw $t6,24($sp)
+	sw $t7,28($sp)
+	
+	li $t0,0xff0000 #vermelho  
+	li $t1,0xB8860B #marrom  
+	li $t2,0x008000 #verde  
+	li $t3,0x006400 #verde escuro  
+	li $t4,0x90EE90 #verde claro 
+	li $t5,0xFFFAFA #gelo 
+	li $t6,0xffff00	#amarelo
+	li $t7,0x000000 #preto	
+	li $t8,0x595959 #cinza	
+	li $t9,0xffaa00 #laranja
+	li $s1,0x86592d #marrom escuro
+				
+	add $s0,$gp,$v0 #Atribui ao registrador $s0 o endereco do pixel central da maca
+		
+        sw $t6,-5640($s0)
+        sw $t6,-5128($s0)
+        sw $t6,-4616($s0)
+        sw $t6,-6160($s0)
+        sw $t6,-6156($s0)
+        sw $t6,-5648($s0)
+        sw $t6,-5644($s0)
+        sw $t6,-5136($s0)
+        sw $t6,-5132($s0)
+        sw $t6,-4624($s0)
+        sw $t6,-4620($s0)
+        sw $t6,-5620($s0)
+        sw $t6,-5616($s0)
+        sw $t6,-5612($s0)
+        sw $t6,-5108($s0)
+        sw $t6,-5104($s0)
+        sw $t6,-5100($s0)
+        sw $t6,-4596($s0)
+        sw $t6,-4592($s0)
+        sw $t6,-4588($s0)
+        sw $t6,-1540($s0)
+        sw $t6,-1536($s0)
+        sw $t6,-1532($s0)
+        sw $t6,-1028($s0)
+        sw $t6,-1024($s0)
+        sw $t6,-1020($s0)
+        sw $t6,-516($s0)
+        sw $t6,-512($s0)
+        sw $t6,-508($s0)
+        sw $t6,-5652($s0)
+        sw $t6,-5140($s0)
+        sw $t6,-5624($s0)
+        sw $t6,-5112($s0)
+        sw $t6,-6160($s0)
+        sw $t6,-6156($s0)
+        sw $t6,-6132($s0)
+        sw $t6,-6128($s0)
+        sw $t6,-2052($s0)
+        sw $t6,-2048($s0)
+        sw $t6,-1544($s0)
+        sw $t6,-1032($s0)
+
+        sw $t9,-6676($s0)
+        sw $t9,-6164($s0)
+        sw $t9,-6168($s0)
+        sw $t9,-5656($s0)
+        sw $t9,-5144($s0)
+        sw $t9,-4628($s0)
+        sw $t9,-4112($s0)
+        sw $t9,-4108($s0)
+        sw $t9,-4104($s0)
+        sw $t9,-7184($s0)
+        sw $t9,-7180($s0)
+        sw $t9,-6672($s0)
+        sw $t9,-6668($s0)
+        sw $t9,-6664($s0)
+        sw $t9,-6152($s0)
+        sw $t9,-6148($s0)
+        sw $t9,-5636($s0)
+        sw $t9,-5124($s0)
+        sw $t9,-4612($s0)
+        sw $t9,-6140($s0)
+        sw $t9,-5628($s0)
+        sw $t9,-5116($s0)
+        sw $t9,-6648($s0)
+        sw $t9,-6136($s0)
+        sw $t9,-7156($s0)
+        sw $t9,-7152($s0)
+        sw $t9,-6644($s0)
+        sw $t9,-6640($s0)
+        sw $t9,-6636($s0)
+        sw $t9,-6124($s0)
+        sw $t9,-6120($s0)
+        sw $t9,-5608($s0)
+        sw $t9,-5096($s0)
+        sw $t9,-4584($s0)
+        sw $t9,-4084($s0)
+        sw $t9,-4080($s0)
+        sw $t9,-4076($s0)
+        sw $t9,-4600($s0)
+        sw $t9,-2060($s0)
+        sw $t9,-1548($s0)
+        sw $t9,-1036($s0)
+        sw $t9,-2568($s0)
+        sw $t9,-2056($s0)
+        sw $t9,-3076($s0)
+        sw $t9,-3072($s0)
+        sw $t9,-2564($s0)
+        sw $t9,-2560($s0)
+        sw $t9,-2556($s0)
+        sw $t9,-2044($s0)
+        sw $t9,-1528($s0)
+        sw $t9,-1016($s0)
+        sw $t9,-504($s0)
+        sw $t9,-4($s0)
+        sw $t9,0($s0)
+        sw $t9,4($s0)
+        sw $t9,-520($s0)       
+        
+	lw $t0,0($sp)
+	lw $t1,4($sp)
+	lw $t2,8($sp)
+	lw $t3,12($sp)
+	lw $t4,16($sp)
+	lw $t5,20($sp)
+	lw $t6,24($sp)
+	lw $t7,28($sp)
+	addi $sp,$sp,32
+	jr $ra
+        #########################
+        ###### Apagar Moeda ############
+	Apagar_moeda:
+	#maça
+	addi $sp,$sp,-32
+	sw $t0,0($sp)
+	sw $t1,4($sp)
+	sw $t2,8($sp)
+	sw $t3,12($sp)
+	sw $t4,16($sp)
+	sw $t5,20($sp)
+	sw $t6,24($sp)
+	sw $t7,28($sp)
+	
+	li $t0,0x000000 #vermelho  
+	li $t1,0x000000 #marrom  
+	li $t2,0x000000 #verde  
+	li $t3,0x000000 #verde escuro  
+	li $t4,0x000000 #verde claro 
+	li $t5,0x000000 #gelo 
+	li $t6,0x000000	#amarelo
+	li $t7,0x000000 #preto	
+	li $t8,0x000000 #cinza	
+	li $t9,0x000000 #laranja
+	li $s1,0x000000 #marrom escuro
+				
+	add $s0,$gp,$v0 #Atribui ao registrador $s0 o endereco do pixel central da maca
+		
+        sw $t6,-5640($s0)
+        sw $t6,-5128($s0)
+        sw $t6,-4616($s0)
+        sw $t6,-6160($s0)
+        sw $t6,-6156($s0)
+        sw $t6,-5648($s0)
+        sw $t6,-5644($s0)
+        sw $t6,-5136($s0)
+        sw $t6,-5132($s0)
+        sw $t6,-4624($s0)
+        sw $t6,-4620($s0)
+        sw $t6,-5620($s0)
+        sw $t6,-5616($s0)
+        sw $t6,-5612($s0)
+        sw $t6,-5108($s0)
+        sw $t6,-5104($s0)
+        sw $t6,-5100($s0)
+        sw $t6,-4596($s0)
+        sw $t6,-4592($s0)
+        sw $t6,-4588($s0)
+        sw $t6,-1540($s0)
+        sw $t6,-1536($s0)
+        sw $t6,-1532($s0)
+        sw $t6,-1028($s0)
+        sw $t6,-1024($s0)
+        sw $t6,-1020($s0)
+        sw $t6,-516($s0)
+        sw $t6,-512($s0)
+        sw $t6,-508($s0)
+        sw $t6,-5652($s0)
+        sw $t6,-5140($s0)
+        sw $t6,-5624($s0)
+        sw $t6,-5112($s0)
+        sw $t6,-6160($s0)
+        sw $t6,-6156($s0)
+        sw $t6,-6132($s0)
+        sw $t6,-6128($s0)
+        sw $t6,-2052($s0)
+        sw $t6,-2048($s0)
+        sw $t6,-1544($s0)
+        sw $t6,-1032($s0)
+
+        sw $t9,-6676($s0)
+        sw $t9,-6164($s0)
+        sw $t9,-6168($s0)
+        sw $t9,-5656($s0)
+        sw $t9,-5144($s0)
+        sw $t9,-4628($s0)
+        sw $t9,-4112($s0)
+        sw $t9,-4108($s0)
+        sw $t9,-4104($s0)
+        sw $t9,-7184($s0)
+        sw $t9,-7180($s0)
+        sw $t9,-6672($s0)
+        sw $t9,-6668($s0)
+        sw $t9,-6664($s0)
+        sw $t9,-6152($s0)
+        sw $t9,-6148($s0)
+        sw $t9,-5636($s0)
+        sw $t9,-5124($s0)
+        sw $t9,-4612($s0)
+        sw $t9,-6140($s0)
+        sw $t9,-5628($s0)
+        sw $t9,-5116($s0)
+        sw $t9,-6648($s0)
+        sw $t9,-6136($s0)
+        sw $t9,-7156($s0)
+        sw $t9,-7152($s0)
+        sw $t9,-6644($s0)
+        sw $t9,-6640($s0)
+        sw $t9,-6636($s0)
+        sw $t9,-6124($s0)
+        sw $t9,-6120($s0)
+        sw $t9,-5608($s0)
+        sw $t9,-5096($s0)
+        sw $t9,-4584($s0)
+        sw $t9,-4084($s0)
+        sw $t9,-4080($s0)
+        sw $t9,-4076($s0)
+        sw $t9,-4600($s0)
+        sw $t9,-2060($s0)
+        sw $t9,-1548($s0)
+        sw $t9,-1036($s0)
+        sw $t9,-2568($s0)
+        sw $t9,-2056($s0)
+        sw $t9,-3076($s0)
+        sw $t9,-3072($s0)
+        sw $t9,-2564($s0)
+        sw $t9,-2560($s0)
+        sw $t9,-2556($s0)
+        sw $t9,-2044($s0)
+        sw $t9,-1528($s0)
+        sw $t9,-1016($s0)
+        sw $t9,-504($s0)
+        sw $t9,-4($s0)
+        sw $t9,0($s0)
+        sw $t9,4($s0)
+        sw $t9,-520($s0)       
+        
 	lw $t0,0($sp)
 	lw $t1,4($sp)
 	lw $t2,8($sp)
